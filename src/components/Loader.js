c91a190748e91d3a0d53eec218653a5500c3af9e
Aleksey Trubtisyn <at.8441451@yandex.ru>
2018-07-24 00:33:31 +0300

    improve styles

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Loader = ({}) => (
-  <div class="spinner">
-    <div class="rect1"></div>
-    <div class="rect2"></div>
-    <div class="rect3"></div>
-    <div class="rect4"></div>
-    <div class="rect5"></div>
+  <div className="spinner-container">
+    <div className="spinner">
+      <div className="rect1"></div>
+      <div className="rect2"></div>
+      <div className="rect3"></div>
+      <div className="rect4"></div>
+      <div className="rect5"></div>
+    </div>
   </div>
 );
 
