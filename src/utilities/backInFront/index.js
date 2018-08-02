21af8ba7b64cc8bd4803ad6c8f0890df609458ad
Aleksey Trubtisyn <at.8441451@yandex.ru>
2018-08-03 00:07:53 +0300

    refactor 'back in front' server emulator

--- src/utilities/backInFront/index.js
+++ src/utilities/backInFront/index.js
@@ -8,8 +8,128 @@ import users from './defaultData/users.json';
 import tickets from './defaultData/tickets.json';
 
 /**
+**
 ** 'Back in front': server emulation.
+**
 **/
+let nextProjectId = 3;
+let nextUserId = 5;
+let nextTicketId = 7;
+
+const _projects = loadFromLS('projects') || projects;
+const _users = loadFromLS('users') || users;
+
+/**
+ * Join user and project data with tickets array
+ *
+ * @param   {array} arr tickets array
+ * @param   {number} projectId id of the project
+ *
+ * @returns {array} array of tickets with actual information about project and users
+ */
+const mapTicketsFromJSON = (arr, projectId) => {
+  let resultArr = arr.map(item => {
+    item.reporter = _users.filter(subitem => subitem.id === item.reporterId)[0] || {};
+    item.assignee = _users.filter(subitem => subitem.id === item.assigneeId)[0] || {};
+
+    item.project = _projects.filter(subitem => subitem.id === projectId)[0] || {};
+
+    return item;
+  })
+
+  return _groupBy(resultArr, 'status');
+}
+
+/**
+ * Projects getter
+ *
+ * @returns {array} array of projects
+ */
+const getProjects = () => _projects;
+
+/**
+ * Users getter
+ *
+ * @returns {array} array of users
+ */
+const getUsers = () => _users;
+
+/**
+ * Default tickets getter (reset board)
+ *
+ * @returns {array} array of tickets by default
+ */
+const getDefaultTickets = (projectId) => mapTicketsFromJSON(tickets, 1);
+
+/**
+** --- Getters ---
+**/
+
+/**
+ * Tickets getter: load from the localStorage
+ * or return array of tickets by default
+ *
+ * @returns {array} array of tickets
+ */
+const getTickets = (projectId) => {
+  let arr = loadFromLS('tickets') || tickets;
+
+  if (!Array.isArray(arr) || !arr.length) return [];
+
+  return mapTicketsFromJSON(arr, projectId);
+};
+
+/**
+** --- Setters ---
+**/
+
+/**
+ * Projects setter: save data to the localStorage
+ * and report a result
+ *
+ * @param   {array} data to save
+ *
+ * @returns {boolean} true if succeed
+ */
+const setProjects = (data) => !!saveToLS('projects', data);
+
+/**
+ * Users setter: save data to the localStorage
+ * and report a result
+ *
+ * @param   {array} data to save
+ *
+ * @returns {boolean} true if succeed
+ */
+const setUsers = (data) => !!saveToLS('users', data);
+
+/**
+ * Tickets setter: save data to the localStorage
+ * and report a result
+ *
+ * @param   {array} data to save
+ *
+ * @returns {boolean} true if succeed
+ */
+const setTickets = (data) => {
+  if (_isEmpty(data)) return !!saveToLS('tickets', []);
+
+  let arr = Object.keys(data).reduce((result, item) => result.concat(data[item]), []);
+
+  return !!saveToLS('tickets', arr);
+}
+
+/**
+** --- export ---
+**/
+
+/**
+ * Main getter
+ *
+ * @param   {string} url of the fake API that contains model name
+ *
+ * @returns {array} array of objects
+ */
 export const _get = (url) => {
   const model = (url.match(/^(\/)([^/?]*)/) || [])[2];
 
@@ -34,6 +154,14 @@ export const _get = (url) => {
   }
 }
 
+/**
+ * Main setter
+ *
+ * @param   {string} url of the fake API that contains model name
+ * @param   {array} data array of objects to save
+ *
+ * @returns {array} array of objects
+ */
 export const _post = (url, data) => {
   const model = (url.match(/^(\/)([^/?]*)/) || [])[2];
 
@@ -50,43 +178,4 @@ export const _post = (url, data) => {
     default:
       return null;
   }
-}
-
-const _projects = loadFromLS('projects') || projects;
-const _users = loadFromLS('users') || users;
-
-const mapTicketsFromJSON = (arr, projectId) => {
-  arr.map(item => {
-    item.author = _users.filter(subitem => subitem.id === item.reporterId)[0] || {};
-    item.developer = _users.filter(subitem => subitem.id === item.assigneeId)[0] || {};
-
-    let _project = _projects.filter(subitem => subitem.id === projectId)[0] || {};
-    item.projectKey = !_isEmpty(_project) ? _project.key : '';
-
-    return item;
-  })
-
-  return _groupBy(arr, 'status');
-}
-
-const getProjects = () => _projects;
-const getUsers = () => _users;
-const getDefaultTickets = (projectId) => mapTicketsFromJSON(tickets, 1);
-
-const getTickets = (projectId) => {
-  let arr = loadFromLS('tickets') || tickets;
-
-  if (!Array.isArray(arr) || !arr.length) return [];
-
-  return mapTicketsFromJSON(arr, projectId);
-};
-
-const setProjects = (data) => !!saveToLS('projects', data);
-const setUsers = (data) => !!saveToLS('users', data);
-const setTickets = (data) => {
-  if (_isEmpty(data)) return !!saveToLS('tickets', []);
-
-  let arr = Object.keys(data).reduce((result, item) => result.concat(data[item]), []);
-
-  return !!saveToLS('tickets', arr);
 }
