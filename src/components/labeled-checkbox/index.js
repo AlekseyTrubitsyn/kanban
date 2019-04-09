9c8a7c816c9a0e5183769548293a82d4f0997d62
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-04-09 14:43:31 +0300

    Create LabeledCheckbox component

---
+++ src/components/labeled-checkbox/index.js
@@ -0,0 +1,3 @@
+export { default } from './labeled-checkbox';
+
+require('./labeled-checkbox.scss');
---
+++ src/components/labeled-checkbox/labeled-checkbox.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const propTypes = {
+  parentClassName: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  containerRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }).isRequired,
+  inputRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }).isRequired,
+  onChange: PropTypes.func,
+  labelText: PropTypes.string
+}
+
+const defaultProps = {
+  parentClassName: '',
+  onChange: null
+}
+
+const LabeledCheckbox = (props) => {
+  const {
+    parentClassName,
+    id,
+    containerRef,
+    inputRef,
+    onChange,
+    labelText,
+  } = props;
+
+  return (
+    <label
+      className={`${parentClassName} labeled-checkbox`}
+      ref={containerRef}
+    >
+      <input
+        className="labeled-checkbox__checkbox checkbox"
+        id={id}
+        ref={inputRef}
+        type="checkbox"
+        onChange={(e) => onChange({ id: "termsAgreementCheckbox", value: e.target.checked })}
+      />
+      <span className="labeled-checkbox__icon checkbox-icon checkbox-icon--checked">
+        <FontAwesomeIcon icon="check" />
+      </span>
+      <span className="labeled-checkbox__icon checkbox-icon checkbox-icon--unchecked">
+        <FontAwesomeIcon icon={["far", "square"]} />
+      </span>
+      <span className="labeled-checkbox__text noselect">
+        {labelText}
+      </span>
+    </label>
+  )
+}
+
+LabeledCheckbox.propTypes = propTypes;
+LabeledCheckbox.defaultProps = defaultProps;
+export default LabeledCheckbox;
